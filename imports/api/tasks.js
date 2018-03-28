2d6dc703a2b0d09b539fa37447549bb7bc537eed
ca-montenegro <ca.montenegro@uniandes.edu.co>
2018-03-28 18:05:42 -0500

    security methods

--- imports/api/tasks.js
+++ imports/api/tasks.js
@@ -5,6 +5,17 @@ import { check } from "meteor/check";
 
 export const Tasks = new Mongo.Collection("tasks");
 
+if(Meteor.isServer){
+    Meteor.publish("tasks", function tasksPublication(){
+        return Tasks.find({
+            $or :[
+                {private:{$ne:true}},
+                {owner:this.userId},
+            ],
+        });
+    });
+}
+
 Meteor.methods({
     "tasks.insert"(text){
         check(text, String);
@@ -24,12 +35,29 @@ Meteor.methods({
     },
     'tasks.remove'(taskId){
         check(taskId, String);
+        const task = Tasks.findOne(taskId);
+        if(task.private && task.owner !== this.userId){
+            throw new Meteor.Error('Not-authorized');
+        }
         Tasks.remove(taskId);
     },
     "tasks.setChecked"(taskId, setChecked){
         check(taskId, String);
         check(setChecked, Boolean);
 
+
         Tasks.update(taskId, {$set:{checked:setChecked}});
+    },
+
+    "tasks.setPrivate"(taskId,setToPrivate){
+        check(taskId, String);
+        check(setToPrivate, Boolean);
+
+        const task = Tasks.findOne(taskId);
+        if(task.private && task.owner !== this.userId){
+            throw new Meteor.Error('Not-authorized');
+        }
+
+        Tasks.update(taskId, {$set: {private:setToPrivate } });
     },
 });
--- imports/ui/Task.js
+++ imports/ui/Task.js
@@ -1,5 +1,6 @@
 import React,{ Component } from "react";
 import { Tasks } from "../api/tasks.js";
+import classnames from "classnames";
 
 export default class Task extends Component{
 
@@ -10,9 +11,16 @@ export default class Task extends Component{
     deleteThisTask(){
         Meteor.call('tasks.remove', this.props.task._id);
     }
+
+    toggledPrivate(){
+        Meteor.call("tasks.setPrivate", this.props.task._id, ! this.props.task.private);
+    }
     render(){
 
-        const taskClassName = this.props.task.checked ? "checked":"";
+        const taskClassName = classnames({
+            checked:this.props.task.checked,
+            private:this.props.task.private,
+        });
         return(
             <li className={taskClassName}>
                 <button className="delete" onClick={this.deleteThisTask.bind(this)}>
@@ -22,6 +30,12 @@ export default class Task extends Component{
                 readOnly
                 checked={!!this.props.task.checked}
                 onClick={this.toggledChecked.bind(this)}/>
+                {this.props.showPrivateButton ? (
+                    <button className="toggle-private" onClick={this.toggledPrivate.bind(this)}>
+                        {this.props.task.private ? "Private":"Public"}
+                    </button>
+                ) : " "}
+
 
                 <span className="text">
                     <strong>{this.props.task.username}</strong>:{this.props.task.text}</span>
